d8cffab826e16a261b03e98ebd19541f950cb4b4
Yasha <3-yasha@users.noreply.gitlab.bbdev.team>
2025-01-26 06:09:04 +0100

    pipeline gh action

--- pack-extension.sh
+++ pack-extension.sh
@@ -4,7 +4,19 @@
 source .env
 
 # Pack the extension
-$CHROME_PATH --pack-extension=$EXTENSION_DIR --pack-extension-key=$OUTPUT_DIR/key.pem
+
+echo "Packing extension..."
+
+# Create the output directory
+mkdir -p $OUTPUT_DIR
+
+if [ ! -f $OUTPUT_DIR/key.pem ]; then
+    echo "Creating key.pem..."
+    $CHROME_PATH --pack-extension=$EXTENSION_DIR
+    mv $EXTENSION_DIR.pem $OUTPUT_DIR/key.pem
+    mv $EXTENSION_DIR.crx $OUTPUT_DIR/$EXTENSION_NAME.crx
+else
+    $CHROME_PATH --pack-extension=$EXTENSION_DIR --pack-extension-key=$OUTPUT_DIR/key.pem
+fi
 
 echo "Extension packed successfully!"
- 
